9247d5b841657d54d6a3c0880e2e0730414a4e1a
VadimKostin <vadkostinxm@gmail.com>
2023-03-09 21:28:16 +0200

    Add Initial

---
+++ REST_API_Example/REST_API_Example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace REST_API_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
